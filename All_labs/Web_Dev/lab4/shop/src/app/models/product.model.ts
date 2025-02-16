3d9c31685f2621231d4807c3bd27b1d48719edc5
Trollninja228 <timur5667262005@gmail.com>
2025-02-16 19:03:44 +0500

    lab4 WD

--- All_labs/Web_Dev/lab4/shop/src/app/models/product.model.ts
+++ All_labs/Web_Dev/lab4/shop/src/app/models/product.model.ts
@@ -1,9 +1,9 @@
 export interface Product {
     id: number;
-    image: string;         // URL главного изображения товара
-    name: string;          // Название товара
-    description: string;   // Описание товара
-    rating: number;        // Рейтинг товара (например, от 0 до 5)
-    link: string;          // Ссылка на товар на kaspi.kz
-    gallery?: string[];    // Опциональное свойство – массив изображений для галереи
+    image: string;        
+    name: string;         
+    description: string;   
+    rating: number;
+    link: string;
+    gallery?: string[];
   }
