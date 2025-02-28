6d7f0356cd699c7d098275cc30c4ac04301fe750
Trollninja228 <timur5667262005@gmail.com>
2025-02-28 15:33:33 +0500

    tr

--- All_labs/OOP/lab5_/origin/Test.java
+++ All_labs/OOP/lab5_/origin/Test.java
@@ -16,13 +16,15 @@ public class Test {
     }
     public static void main(String[] args){
         Employee emp=new Employee(101, "Jane Smith", "012-34-5678", 120_345.27);
-        Manager emp1=new Manager(207,"Barbara Johnson","054-12-2367",109_501.36,"US Marketing", new Employee[10]);
-        Admin emp2=new Admin(304,"Bill Munroe","108-23-6509",75_002.34);
+        Employee emp1=new Manager(207,"Barbara Johnson","054-12-2367",109_501.36,"US Marketing", new Employee[10]);
+        Employee emp2=new Admin(304,"Bill Munroe","108-23-6509",75_002.34);
         Director emp3=new Director(12,"Susan Wheeler","099-45-2340",120_567.36,"Global Marketing",1_000_000.00);
 
         printEmployee(emp,new EmployeeStockPlan());
 
     }
+    
+    
 
 
     
